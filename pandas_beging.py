140d20cacea4d19b9c1ad0e2c82e65c8316e32d4
Petri Lamminaho <pelammin77@gmail.com>
2017-11-05 20:13:22 +0200

    first simple df script

---
+++ pandas_beging.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import datetime
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+start_date = datetime.datetime(2014,1,1)
+end_date = datetime.datetime(2015,1,1)
+
+#df = web.DataReader('XOM', "yahoo", start_date, end_date)
+
+df = web.DataReader("XOM", "yahoo", start_date, end_date)
+print(df.head())
+print(df.tail())
+
+df['High'].plot()
+plt.legend()
+plt.show()
